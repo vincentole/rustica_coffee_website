633505afe0ebb9e0f71c46e23b6bb64afdbb4137
vincentole <63076449+vincentole@users.noreply.github.com>
2022-01-20 18:53:28 +0100

    added data to data array

--- data/shopItemsData.ts
+++ data/shopItemsData.ts
@@ -70,11 +70,43 @@ const shopItemsData = [
 
         aroma: 'Sehr mild',
         cookingUtilities: ['French Press'],
-        allVariants: ['gemahlen', 'Bohnen'],
+        allVariants: ['Bohnen'],
+        allWeights: [250, 500],
+        prices: [11, 20],
+        summary:
+            'Brazil Filterkaffee ist äußerst ergiebig und hat praktisch keine Säure. Den Gaumen umschmeichelt ein vollmundiges Haselnussaroma. Gleich probieren!',
+        description:
+            'MAYA Hochlandkaffee von MAYA Kaffee 1991 wird an den Hängen der Sierra Madre de Chiapas angebaut, ein Gebirgszug, der sich entlang der Pazifikküste im Süden Mexikos erstreckt. Hier haben sich indianische Kleinbauern mit Anbauflächen von ein bis drei Hektar Land in Kooperativen zusammengeschlossen und verzichten komplett auf Industriedünger und Pestizide. Schonend geröstet, entwickelt diese einzigartige Spezialität ein überraschend vielschichtiges Aromenspiel mit deutlichen Noten von Haselnuss. Als klassischer Aufguss ist unser MAYA Filterkaffee besonders ergiebig. 100% BIO aus kontrolliert ökologischem Anbau. DE-ÖKO-003 / Nicht-EU-Landwirtschaft.',
+        readyForDelivery: true,
+        img: coffeeCostaRicaImage,
+    },
+    {
+        id: 'columbia',
+        title: 'Columbia',
+
+        aroma: 'Stark',
+        cookingUtilities: ['French Press'],
+        allVariants: ['gemahlen'],
         allWeights: [250, 500],
         prices: [9, 18],
         summary:
-            'Brazil Filterkaffee ist äußerst ergiebig und hat praktisch keine Säure. Den Gaumen umschmeichelt ein vollmundiges Haselnussaroma. Gleich probieren!',
+            'Columbia Filterkaffee ist äußerst ergiebig und hat praktisch keine Säure. Den Gaumen umschmeichelt ein vollmundiges Haselnussaroma. Gleich probieren!',
+        description:
+            'MAYA Hochlandkaffee von MAYA Kaffee 1991 wird an den Hängen der Sierra Madre de Chiapas angebaut, ein Gebirgszug, der sich entlang der Pazifikküste im Süden Mexikos erstreckt. Hier haben sich indianische Kleinbauern mit Anbauflächen von ein bis drei Hektar Land in Kooperativen zusammengeschlossen und verzichten komplett auf Industriedünger und Pestizide. Schonend geröstet, entwickelt diese einzigartige Spezialität ein überraschend vielschichtiges Aromenspiel mit deutlichen Noten von Haselnuss. Als klassischer Aufguss ist unser MAYA Filterkaffee besonders ergiebig. 100% BIO aus kontrolliert ökologischem Anbau. DE-ÖKO-003 / Nicht-EU-Landwirtschaft.',
+        readyForDelivery: true,
+        img: coffeeCostaRicaImage,
+    },
+    {
+        id: 'uruguay',
+        title: 'Uruguay',
+
+        aroma: 'Stark',
+        cookingUtilities: ['French Press'],
+        allVariants: ['gemahlen'],
+        allWeights: [250, 500],
+        prices: [9, 18],
+        summary:
+            'Uruguay Filterkaffee ist äußerst ergiebig und hat praktisch keine Säure. Den Gaumen umschmeichelt ein vollmundiges Haselnussaroma. Gleich probieren!',
         description:
             'MAYA Hochlandkaffee von MAYA Kaffee 1991 wird an den Hängen der Sierra Madre de Chiapas angebaut, ein Gebirgszug, der sich entlang der Pazifikküste im Süden Mexikos erstreckt. Hier haben sich indianische Kleinbauern mit Anbauflächen von ein bis drei Hektar Land in Kooperativen zusammengeschlossen und verzichten komplett auf Industriedünger und Pestizide. Schonend geröstet, entwickelt diese einzigartige Spezialität ein überraschend vielschichtiges Aromenspiel mit deutlichen Noten von Haselnuss. Als klassischer Aufguss ist unser MAYA Filterkaffee besonders ergiebig. 100% BIO aus kontrolliert ökologischem Anbau. DE-ÖKO-003 / Nicht-EU-Landwirtschaft.',
         readyForDelivery: true,
